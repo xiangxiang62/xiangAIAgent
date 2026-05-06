41b96a22225f655eeeefd22733da6ae468a0a459
张鑫 <1964362087@qq.com>
2026-05-06 11:36:23 +0800

    feat: 后端项目初始化

---
+++ xiangAIAgent-backend/src/main/java/com/xiang/ai/xiangAIAgent/XiangAIAgentApplication.java
@@ -0,0 +1,19 @@
+package com.xiang.ai.xiangAIAgent;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync // 开启异步调用
+@MapperScan("com.xiang.ai.xiangAIAgent.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true) // 开启 AOP
+public class XiangAIAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiangAIAgentApplication.class, args);
+    }
+
+}
---
+++ xiangAIAgent-backend/src/test/java/xiangPicBackend/XiangPicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xiangAgentBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class xiangAgentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
