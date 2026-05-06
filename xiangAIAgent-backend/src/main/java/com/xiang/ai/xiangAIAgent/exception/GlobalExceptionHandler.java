b40be2cb8c440471a651a525e66d53a8991ecbcd
张鑫 <1964362087@qq.com>
2026-05-06 11:36:48 +0800

    feat: 后端项目初始化

--- xiangAIAgent-backend/src/main/java/com/xiang/ai/xiangAIAgent/exception/GlobalExceptionHandler.java
+++ xiangAIAgent-backend/src/main/java/com/xiang/ai/xiangAIAgent/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.xiang.ai.xiangAIAgent.exception;
 
-import cn.dev33.satoken.exception.NotLoginException;
-import cn.dev33.satoken.exception.NotPermissionException;
 import com.xiang.ai.xiangAIAgent.common.BaseResponse;
 import com.xiang.ai.xiangAIAgent.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -15,19 +13,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(NotLoginException.class)
-    public BaseResponse<?> notLoginException(NotLoginException e) {
-        log.error("NotLoginException", e);
-        return ResultUtils.error(ErrorCode.NOT_LOGIN_ERROR, e.getMessage());
-    }
-
-    @ExceptionHandler(NotPermissionException.class)
-    public BaseResponse<?> notPermissionExceptionHandler(NotPermissionException e) {
-        log.error("NotPermissionException", e);
-        return ResultUtils.error(ErrorCode.NO_AUTH_ERROR, e.getMessage());
-    }
-
 
     @ExceptionHandler(BusinessException.class)
     public BaseResponse<?> businessExceptionHandler(BusinessException e) {
