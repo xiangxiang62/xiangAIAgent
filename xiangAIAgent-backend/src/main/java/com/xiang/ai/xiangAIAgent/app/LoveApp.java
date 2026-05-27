10a95a42907dfb619416947384d2893a502d0ee8
张鑫 <1964362087@qq.com>
2026-05-27 14:02:27 +0800

    feat: 添加自定义日志 advisor

--- xiangAIAgent-backend/src/main/java/com/xiang/ai/xiangAIAgent/app/LoveApp.java
+++ xiangAIAgent-backend/src/main/java/com/xiang/ai/xiangAIAgent/app/LoveApp.java
@@ -1,5 +1,6 @@
 package com.xiang.ai.xiangAIAgent.app;
 
+import com.xiang.ai.xiangAIAgent.advisor.MyLoggerAdvisor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -21,13 +22,12 @@ public class LoveApp {
 
     private final ChatClient chatClient;
 
-    /**
-     * 系统提示词
-     */
     private static final String SYSTEM_PROMPT = """
             扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。
-            围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；
-            恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。
+            围绕单身、恋爱、已婚三种状态提问：
+            单身状态询问社交圈拓展及追求心仪对象的困扰；
+            恋爱状态询问沟通、习惯差异引发的矛盾；
+            已婚状态询问家庭责任与亲属关系处理的问题。
             引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。
             """;
 
@@ -42,11 +42,11 @@ public class LoveApp {
         chatClient = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory)
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new MyLoggerAdvisor()
                 )
                 .build();
     }
-
 
     /**
      * 聊天
@@ -56,16 +56,20 @@ public class LoveApp {
      * @return
      */
     public String doChat(String message, String chatId) {
+
         ChatResponse response = chatClient
                 .prompt()
                 .user(message)
-                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                .advisors(spec -> spec
+                        .param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
                         .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
                 .call()
                 .chatResponse();
+
         String content = response.getResult().getOutput().getText();
+
         log.info("content: {}", content);
+
         return content;
     }
-
 }
